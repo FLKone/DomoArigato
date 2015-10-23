1a9c83262532b373a612912608f97566d4b0bccf
FLKone <FLKone@users.noreply.github.com>
2015-10-23 04:52:55 +0200

    Devices are now stored in Core Data

--- DomoArigato/MainViewController.swift
+++ DomoArigato/MainViewController.swift
@@ -7,25 +7,43 @@
 //
 
 import UIKit
+import CoreData
 import Alamofire
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var ReloadButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
-    var items: [NSDictionary] = []// = ["We", "Heart", "Swift"]
+    var devices =  [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         
         self.refreshData(nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest(entityName: "Device")
+        
+        do {
+            let results =
+            try managedContext.executeFetchRequest(fetchRequest)
+            devices = results as! [NSManagedObject]
+            
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     
@@ -36,55 +54,41 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count;
+        return devices.count;
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-        let cell = self.tableView.dequeueReusableCellWithIdentifier("BasicCell", forIndexPath: indexPath) as UITableViewCell!
-        cell.textLabel?.text = self.items[indexPath.row].objectForKey("Name") as? String
-        let data = self.items[indexPath.row].objectForKey("Data") as? String
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("BasicCell", forIndexPath: indexPath)
         
-        cell.detailTextLabel?.text = data
+        let device = devices[indexPath.row]
         
-        
-        if (self.items[indexPath.row].objectForKey("TypeImg") as? String == "lightbulb") {
-            print("row: \(cell.textLabel?.text) light : \(data)")
+        cell.textLabel!.text = device.valueForKey("name") as? String
+        cell.detailTextLabel!.text = device.valueForKey("data") as? String
+
+        if (device.valueForKey("type") as? String == "lightbulb") {
+            print("row: \(cell.textLabel?.text) light : \(device.valueForKey("data"))")
         }
-        else if (self.items[indexPath.row].objectForKey("TypeImg") as? String == "temperature") {
-            print("row: \(cell.textLabel?.text) temp : \(data)")
+        else if (device.valueForKey("type") as? String == "temperature") {
+            print("row: \(cell.textLabel?.text) temp : \(device.valueForKey("data"))")
 
         }
-        
-         /*
-        let dic = self.items[indexPath.row] as [String:AnyObject]
-        cell!.textLabel?.text = dic.objectForKey("name")
-        
-       
-        if dic = self.items[indexPath.row] {
-            if let name = dic.objectForKey("name") as String {
-                cell!.textLabel?.text = name
-            }
-        }
-*/
 
-        
-        return cell!
-        
-        
+        return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        print("You selected cell #\(indexPath.row)!", terminator: "")
+        print("You selected cell #\(indexPath.row)!")
     }
     
 
     @IBAction func refreshData(sender: AnyObject?) {
-        print("refreshData from \(sender)", terminator: "")
+        print("refreshData from \(sender)")
         
-        self.items.removeAll()
+        //self.items.removeAll()
+        
         
         Alamofire.request(.GET, "http://192.168.1.29:8080/json.htm?type=devices&filter=all&used=true&order=Name")
             .responseJSON { response in
@@ -94,17 +98,76 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                 //print(response.result)   // result of response serialization
                 
                 if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
+                    //print("JSON: \(JSON)")
+                    
+                    //Core Data
+                    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                    let managedContext = appDelegate.managedObjectContext
+                    let entity =  NSEntityDescription.entityForName("Device", inManagedObjectContext:managedContext)
+                    
                     
                     if let result = JSON["result"] as? NSArray {
                         for item in result {
-                            let obj = item as? NSDictionary
-                            if let isFavorite = obj?.objectForKey("Favorite") {
-                                if (isFavorite.integerValue == 1) {
-                                    self.items.append(obj!)
-                                    //print(obj)
+                            let obj = item as! NSDictionary
+                    
+                            let request = NSFetchRequest(entityName: "Device")
+                            request.predicate = NSPredicate(format: "id == %@", argumentArray: [(obj.valueForKey("ID"))!])
+                            
+                            do {
+                                let result = try managedContext.executeFetchRequest(request) as NSArray
+                                
+                                //print("Fetch ok: \(result)")
+                                if result.count == 2 {
+                                    continue
                                 }
+                                    
+                                if result.count == 1 {
+                                    //update
+                                    let device = result.objectAtIndex(0) as! NSManagedObject
+                                    let olddevice = device
+
+
+                                    device.setValue(obj.valueForKey("Name"), forKey: "name")
+                                    device.setValue(obj.valueForKey("ID"), forKey: "id")
+                                    device.setValue(obj.valueForKey("TypeImg"), forKey: "type")
+                                    device.setValue(obj.valueForKey("Data"), forKey: "data")
+                                    device.setValue("\(obj.valueForKey("Favorite")!)", forKey: "isFavorite")
+                                    
+                                    //Save It
+                                    do {
+                                        try managedContext.save()
+                                        self.devices[self.devices.indexOf(olddevice)!] = device
+                                        
+                                    } catch let error as NSError  {
+                                        print("Could not save \(error), \(error.userInfo)")
+                                    }
+                                }
+                                else if result.count == 0 {
+                                    //create new one
+                                    let device = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+                                    device.setValue(obj.valueForKey("Name"), forKey: "name")
+                                    device.setValue(obj.valueForKey("ID"), forKey: "id")
+                                    device.setValue(obj.valueForKey("TypeImg"), forKey: "type")
+                                    device.setValue(obj.valueForKey("Data"), forKey: "data")
+                                    device.setValue("\(obj.valueForKey("Favorite")!)", forKey: "isFavorite")
+
+                                    //Save It
+                                    do {
+                                        try managedContext.save()
+                                        self.devices.append(device)
+                                        
+                                    } catch let error as NSError  {
+                                        print("Could not save \(error), \(error.userInfo)")
+                                        
+                                    }
+                                }
+                                
+                                
+                            } catch let error as NSError {
+                                print("Fetch failed: \(error.localizedDescription)")
+
                             }
+
                         }
                     }
                     
@@ -113,7 +176,5 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                 }
         }
     }
-    
-    
 }
 
