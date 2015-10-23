044b7b2b7676eff67e3e546215d00d1fe2304fe5
FLKone <FLKone@users.noreply.github.com>
2015-10-23 22:44:46 +0200

    Devices grouped by type

--- DomoArigato/MainViewController.swift
+++ DomoArigato/MainViewController.swift
@@ -15,31 +15,71 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var ReloadButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     var devices =  [NSManagedObject]()
+    var showFavoritesOnly: Bool = false
+    
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        let animalsFetchRequest = NSFetchRequest(entityName: "Device")
+
+        let primarySortDescriptor = NSSortDescriptor(key: "type", ascending: true)
+        let secondarySortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+
+        animalsFetchRequest.sortDescriptors = [primarySortDescriptor, secondarySortDescriptor]
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let frc = NSFetchedResultsController(
+            fetchRequest: animalsFetchRequest,
+            managedObjectContext: appDelegate.managedObjectContext,
+            sectionNameKeyPath: "type",
+            cacheName: nil)
+        
+        frc.delegate = self
+        
+        return frc
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        self.refreshData(nil)
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("An error occurred")
+        }
+
+        //self.refreshData(nil)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+        /*
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
         
         let fetchRequest = NSFetchRequest(entityName: "Device")
-        
+        let entity =  NSEntityDescription.entityForName("Device", inManagedObjectContext:managedContext)
+
+        let typeDesc = entity!.attributesByName["type"]
+        let nameDesc = entity!.attributesByName["name"]
+        let dataDesc = entity!.attributesByName["data"]
+
+        //[fetch setPropertiesToFetch:[NSArray arrayWithObjects:statusDesc, expressionDescription, nil]];
+        fetchRequest.propertiesToFetch = [typeDesc!, nameDesc!, dataDesc!]
+        fetchRequest.propertiesToGroupBy = [typeDesc!, nameDesc!, dataDesc!]
+        fetchRequest.resultType = .DictionaryResultType
+
         do {
-            let results =
-            try managedContext.executeFetchRequest(fetchRequest)
-            devices = results as! [NSManagedObject]
+            let results = try managedContext.ex(fetchRequest)
+            print(results)
+            
+            //devices = results as! [NSManagedObject]
             
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
             
         }
+        */
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,17 +90,35 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     // MARK: - UITableView DataSource
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        if let sections = fetchedResultsController.sections {
+            print("sections \(sections.count)")
+            return sections.count
+        }
+        
+        return 0
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return devices.count;
+        if let sections = fetchedResultsController.sections {
+            let currentSection = sections[section]
+            return currentSection.numberOfObjects
+        }
+        
+        return 0
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
         let cell = self.tableView.dequeueReusableCellWithIdentifier("BasicCell", forIndexPath: indexPath)
+
+
+        self.configureCell(cell, atIndexPath: indexPath)
         
+        
+        return cell
+        
+        
+        /*
         let device = devices[indexPath.row]
         
         cell.textLabel!.text = device.valueForKey("name") as? String
@@ -75,12 +133,55 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
 
         return cell
+*/
+    }
+    
+    func configureCell(cell: UITableViewCell,
+        atIndexPath indexPath: NSIndexPath) {
+            
+            let device = fetchedResultsController.objectAtIndexPath(indexPath)
+            cell.textLabel?.text = device.valueForKey("name") as? String
+            cell.detailTextLabel?.text = device.valueForKey("data") as? String
+            
+            //print("row: \(device) data : \(device.valueForKey("type"))")
+            
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let sections = fetchedResultsController.sections {
+            let currentSection = sections[section]
+            return currentSection.name
+        }
+        
+        return nil
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         print("You selected cell #\(indexPath.row)!")
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            print("delete")
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let managedContext = appDelegate.managedObjectContext
+            
+            managedContext.deleteObject(fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject)
+            
+            //self.fetchedResultsController.delete(fetchedResultsController.objectAtIndexPath(indexPath))
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
+        else {
+            print("commit \(editingStyle)")
+        }
     }
     
 
@@ -124,8 +225,6 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                                 if result.count == 1 {
                                     //update
                                     let device = result.objectAtIndex(0) as! NSManagedObject
-                                    let olddevice = device
-
 
                                     device.setValue(obj.valueForKey("Name"), forKey: "name")
                                     device.setValue(obj.valueForKey("ID"), forKey: "id")
@@ -136,7 +235,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                                     //Save It
                                     do {
                                         try managedContext.save()
-                                        self.devices[self.devices.indexOf(olddevice)!] = device
+                                        //self.devices[self.devices.indexOf(olddevice)!] = device
                                         
                                     } catch let error as NSError  {
                                         print("Could not save \(error), \(error.userInfo)")
@@ -154,7 +253,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                                     //Save It
                                     do {
                                         try managedContext.save()
-                                        self.devices.append(device)
+                                        //self.devices.append(device)
                                         
                                     } catch let error as NSError  {
                                         print("Could not save \(error), \(error.userInfo)")
@@ -171,10 +270,75 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                         }
                     }
                     
-                    self.tableView.reloadData()
+                    //self.tableView.reloadData()
                     
                 }
         }
+    }
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        print("====== willChangeContent")
+        self.tableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        print("Section \(sectionIndex) at \(type)")
+        
+        switch type {
+            case .Insert:
+                print("Insert")
+                self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+            case .Update:
+                print("Update")
+            case .Move:
+                print("Move")
+            case .Delete:
+                print("Delete")
+                self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        }
+
+
+    }
+    
+    func controller(controller: NSFetchedResultsController,
+        didChangeObject object: AnyObject,
+        atIndexPath indexPath: NSIndexPath?,
+        forChangeType type: NSFetchedResultsChangeType,
+        newIndexPath: NSIndexPath?) {
+            
+            print("Object \(object) at \(indexPath)")
+            
+            switch type {
+                case .Insert:
+                    print("Insert")
+                    self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+                case .Update:
+                    print("Update")
+                    let cell = self.tableView.cellForRowAtIndexPath(indexPath!)
+                    self.configureCell(cell!, atIndexPath: indexPath!)
+                    self.tableView.reloadRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+                case .Move:
+                    print("Move")
+                    self.tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+                    self.tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+                case .Delete:
+                    print("Delete \(indexPath)")
+                    let rows = self.tableView(tableView, numberOfRowsInSection: indexPath!.section)
+                    print(" rows= \(rows)")
+                    self.tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+                    if rows == 0 {
+                        self.tableView.deleteSections(NSIndexSet(index: indexPath!.section), withRowAnimation: .Fade)
+                    }
+
+            }
+    }
+    
+    /* called last
+    tells `UITableView` updates are complete */
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        print("didChangeContent")
+        self.tableView.endUpdates()
     }
 }
 
